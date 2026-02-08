e793d9a175ea3fe8b0310dffc7e7be758780027b
DELL <jatin.careeer@gmail.com>
2026-02-08 23:20:51 +0530

    new learn

--- First/src/Sum.java
+++ First/src/Sum.java
@@ -4,8 +4,9 @@ public class Sum {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.print("Enter first number:");
         int a = input.nextInt();
+        System.out.print("Enter second number:");
         int b = input.nextInt();
 
         int c = a;
